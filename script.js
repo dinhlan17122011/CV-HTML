205e64fd91193d2a8c029b4d6a43ce1481cb4ebd
dinhlan17122011 <dinhlan17122011@gmail.com>
2024-10-29 09:58:33 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript có thể dùng để thêm tính năng tương tác như load nội dung động
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Trang CV đã được tải");
+});
